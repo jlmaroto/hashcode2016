96e8a7180b16cb185f50e5341f12aeeadf1ad6e2
Ivan Marques <ivan.marques@gmail.com>
2016-02-11 22:18:19 +0100

    sort

--- sort_orders.py
+++ sort_orders.py
@@ -5,21 +5,21 @@ products = []
 
 
 def calculate_weight(item):
-    for p_id, amount in item["items"].iteritems():
-        print p_id,amount
-        print product[p_id]
-            
+    # for p_id, amount in item["items"].iteritems():
+    #     print p_id,amount
+    #     print product[p_id]
+    #
     return item["weight"]
 
 def sort_orders(orders):
-    
+
     return nsmallest(len(orders),orders,key=calculate_weight)
 
 if __name__ == "__main__":
     import load
     sim, products, warehouses, orders = load.read_input('datos/busy_day.in')
 
-    print 
+    print
     orders = sort_orders(orders)
     print
     for o in orders:
